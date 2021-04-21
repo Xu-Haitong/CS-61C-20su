21a7b4109d0aae9347df814e67bcd847d7c30129
Xu Haitong <2452009536@qq.com>
2021-04-21 08:22:10 +0800

    lab01 done

--- lab01/interactive_hello.c
+++ lab01/interactive_hello.c
@@ -2,7 +2,10 @@
 
 #define MAX_LEN 80
 
+
 int main(int argc, char *argv[]) {
+    // freopen() 重定向输入流
+//    freopen("C:\\CODE\\C\\CS 61C\\su20-lab-starter\\lab01\\a.txt", "r", stdin);
     char a_word[MAX_LEN];
 
     printf("What's your name?\n");
--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,15 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL) return 0;
+    node *fast = head;
+    node *slow = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return 1;
+        }
+    }
     return 0;
 }
