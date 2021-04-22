77da61cbda7fc0542d155e8981e3c81c59b2bf4c
Xu Haitong <2452009536@qq.com>
2021-04-22 15:17:47 +0800

    lab02 done

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,8 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t shiftIn = (((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1) << 15;
+    uint16_t shiftRight = *reg >> 1;
+    *reg = shiftIn | shiftRight;
 }
 
