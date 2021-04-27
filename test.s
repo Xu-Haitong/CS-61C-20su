c62eadc0ce6138116e2f270a2bcd1a2cb99948e0
Xu Haitong <2452009536@qq.com>
2021-04-27 21:43:16 +0800

    lab04 done

--- test.s
+++ test.s
@@ -1,112 +1,184 @@
-.globl matmul
+.globl classify
 
 .text
-# =======================================================
-# FUNCTION: Matrix Multiplication of 2 integer matrices
-# 	d = matmul(m0, m1)
-#   The order of error codes (checked from top to bottom):
-#   If the dimensions of m0 do not make sense, 
-#   this function exits with exit code 2.
-#   If the dimensions of m1 do not make sense, 
-#   this function exits with exit code 3.
-#   If the dimensions don't match, 
-#   this function exits with exit code 4.
-# Arguments:
-# 	a0 (int*)  is the pointer to the start of m0 
-#	a1 (int)   is the # of rows (height) of m0
-#	a2 (int)   is the # of columns (width) of m0
-#	a3 (int*)  is the pointer to the start of m1
-# 	a4 (int)   is the # of rows (height) of m1
-#	a5 (int)   is the # of columns (width) of m1
-#	a6 (int*)  is the pointer to the the start of d
-# Returns:
-#	None (void), sets d = matmul(m0, m1)
-# =======================================================
-matmul:
+classify:
+    # =====================================
+    # COMMAND LINE ARGUMENTS
+    # =====================================
+    # Args:
+    #   a0 (int)    argc
+    #   a1 (char**) argv
+    #   a2 (int)    print_classification, if this is zero, 
+    #               you should print the classification. Otherwise,
+    #               this function should not print ANYTHING.
+    # Returns:
+    #   a0 (int)    Classification
+    # 
+    # If there are an incorrect number of command line args,
+    # this function returns with exit code 49.
+    #
+    # Usage:
+    #   main.s -m -1 <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH> 
+    
+    # Windows : java -jar tools\venus.jar src\main.s -ms -1 tests\inputs\mnist\bin\m0.bin tests\inputs\mnist\bin\m1.bin tests\inputs\mnist\bin\inputs\mnist_input3.bin tests\outputs\test_mnist_main\student_mnist_outputs.bin
 
-    # Error checks
-    li t0, 1             # t0 = 1
-    blt a1, t0, exit_2
-    blt a2, t0, exit_2
-    blt a4, t0, exit_3
-    blt a5, t0, exit_3 
-    bne a2, a4, exit_4
+    # Linux : java -jar tools/venus.jar src/main.s -ms -1 tests/inputs/mnist/bin/m0.bin tests/inputs/mnist/bin/m1.bin tests/inputs/mnist/bin/inputs/mnist_input0.bin  tests/outputs/test_mnist_main/student_mnist_outputs.bin
+
+    # verify the number of command line arguments
+    li t0, 5
+    bne a0, t0, exit_49
 
     # Prologue
-    addi sp, sp, -4
-    sw ra, 0(sp)
+    addi sp, sp, -64
+    sw s0, 0(sp)
+	sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw s6, 24(sp)
+    sw s7, 28(sp)
+    sw s8, 32(sp)  
+    sw ra, 36(sp)
 
-    li t0, 0                        # t0 = 0         =>  i = 0
-outer_loop_start:
-    bge t0, a1, outer_loop_end      # if(t0 < a1)    
+    # save arguments
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
 
-    li t1, 0                        # t1 = 0         =>  j = 0
-inner_loop_start:
-    bge t1, a5, inner_loop_end      # if(t1 < a5)    
-    mul t2, t0, a5                  # t2 = i * k
-    add t2, t1, t2                  # t2 = j + i * k
-    slli t2, t2, 2                  # t2 is offset
-    add t2, a6, t2                  # t2 = &d[i][j]
-    addi, sp, sp, -40               # push t0, t1, t2, a0-a6
-    sw t0, 0(sp)
-    sw t1, 4(sp)
-    sw t2, 8(sp)
-    sw a0, 12(sp)
-    sw a1, 16(sp)
-    sw a2, 20(sp)
-    sw a3, 24(sp)
-    sw a4, 28(sp)
-    sw a5, 32(sp)
-    sw a6, 36(sp)
-    mul t3, t0, a2                  # t3 = i * m
-    slli t3, t3, 2                  # t3 is offset
-    add t3, a0, t3                  # t3 = &m0[i][0]
-    slli t4, t1, 2                  # t4 is offset
-    add t4, a3, t4                  # t4 = &m1[0][j]
-    mv t5, a2                       # t5 = m
-    mv t6, a5                       # t6 = k
-    mv a0, t3                       # set parameters for dot(int *, int *, int, int, int)
-    mv a1, t4
-    mv a2, t5
-    li a3, 1
-    mv a4, t6
-    jal ra, dot                     # call dot(int *, int *, int, int, int)
-    sw a0, 0(t2)                    # *(d + j + i * k) = dot(m0 + i * c0, m1 + j, m, 1, c1);
-    lw t0, 0(sp)
-    lw t1, 4(sp)
-    lw t2, 8(sp)
-    lw a0, 12(sp)
-    lw a1, 16(sp)
-    lw a2, 20(sp)
-    lw a3, 24(sp)
-    lw a4, 28(sp)
-    lw a5, 32(sp)
-    lw a6, 36(sp)
-    addi, sp, sp, 40                # pop t0, t1, t2, a0-a6
-    addi t1, t1, 1                  # t1++  =>  j++ 
-    jal x0, inner_loop_start
+	# =====================================
+    # LOAD MATRICES
+    # =====================================
 
-inner_loop_end:
-    addi t0, t0, 1                  # t0++  =>  i++ 
-    jal x0, outer_loop_start        
+    # load m0
+    lw a0, 4(s1)
+    addi a1, sp, 40
+    addi a2, sp, 44
+    jal ra, read_matrix
+    mv s3, a0            # s3 is the pointer to m0
+  
+    # load m1
+    lw a0, 8(s1)
+    addi a1, sp, 48
+    addi a2, sp, 52
+    jal ra, read_matrix
+    mv s4, a0            # s4 is the pointer to m1
 
-outer_loop_end:
-    # Epilogue
-    lw ra, 0(sp)
-    addi sp, sp, 4
+    # load input matrix
+    lw a0, 12(s1)
+    addi a1, sp, 56
+    addi a2, sp, 60
+    jal ra, read_matrix
+    mv s5, a0            # s5 is the pointer to input matrix
+
+    # =====================================
+    # RUN LAYERS
+    # =====================================
+    # 1. LINEAR LAYER:    m0 * input
+    # 2. NONLINEAR LAYER: ReLU(m0 * input)
+    # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
+
+    # malloc for hidden_layer
+    lw t0, 40(sp)
+    lw t1, 60(sp) 
+    mul t0, t0, t1
+    slli t0, t0, 2
+    mv a0, t0
+    jal ra, malloc
+    blt a0, x0, exit_48
+    mv s6, a0            # s6 is the pointer to hidden_layer
+
+    # m0 * input
+    mv a0, s3
+    lw a1, 40(sp)
+    lw a2, 44(sp)
+    mv a3, s5
+    lw a4, 56(sp)
+    lw a5, 60(sp)
+    mv a6, s6
+    jal ra, matmul
+
+    # relu()
+    lw t0, 40(sp)
+    lw t1, 60(sp)
+    mul t0, t0, t1
+    mv a0, s6
+    mv a1, t0
+    jal ra, relu
+
+    # malloc for scores
+    lw t0, 48(sp)
+    lw t1, 60(sp)
+    mul t0, t0, t1
+    slli t0, t0, 2
+    mv a0, t0
+    jal ra, malloc
+    blt a0, x0, exit_48
+    mv s7, a0             # s7 is the pointer to scores
+
+    # m1 * hidden_layer
+    mv a0, s4
+    lw a1, 48(sp)
+    lw a2, 52(sp)
+    mv a3, s6
+    lw a4, 40(sp)
+    lw a5, 60(sp)
+    mv a6, s7
+    jal ra, matmul
+
+    # =====================================
+    # WRITE OUTPUT
+    # =====================================
+    # Write output matrix
+    lw a0, 16(s1)
+    mv a1, s7
+    lw a2, 48(sp)
+    lw a3, 60(sp)
+    jal ra, write_matrix
+
+    # =====================================
+    # CALCULATE CLASSIFICATION/LABEL
+    # =====================================
+    # Call argmax
+    lw t0, 48(sp)
+    lw t1, 60(sp)
+    mv a0, s7
+    mul a1, t0, t1
+    jal ra, argmax
+    mv s8, a0          # s8 is the result
+
+    # Print classification
+    bne s2, x0, end
+    mv a1, s8
+    jal ra, print_int
+
+    # Print newline afterwards for clarity
+    li a1, '\n'
+    jal ra, print_char
+    
+end:
+    # output
+    mv a0, s8
+
+    #Epilogue  
+    lw s0, 0(sp)
+	lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw s6, 24(sp)
+    lw s7, 28(sp)
+    lw s8, 32(sp)  
+    lw ra, 36(sp)
+    addi sp, sp, 64
+
     ret
 
-exit_2:
-    li a0, 17
-    li a1, 2
-    ecall
+exit_49:
+    li a1, 49
+    jal ra, exit2 
 
-exit_3:
-    li a0, 17
-    li a1, 3
-    ecall
-
-exit_4:
-    li a0, 17
-    li a1, 4
-    ecall
+exit_48:
+    li a1, 48
+    j exit2
